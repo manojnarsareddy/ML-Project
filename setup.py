90f581a55a46db1d36f8ad1e293c218dbb006321
manojnarsareddy <nmanoj@gmail.com>
2024-11-08 18:46:06 -0600

    created setup file

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT ="-e ."
+
+def get_requirement(file_path:str)->List[str]:
+
+    """This function will return the list of requirement"""
+
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT) 
+                        
+    
+
+setup(name="ML Project",
+      version =0.01,
+      author="Manoj",
+      author_email="nmanoj@gmail.com",
+      packages=find_packages(),
+      install_requires = get_requirement("Requirement.txt")
+
+)
